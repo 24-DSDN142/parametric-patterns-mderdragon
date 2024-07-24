686dc16553ec17f51b680f8c875025933f90898b
mderdragon <emma.r.nash@gmail.com>
2024-07-25 10:36:04 +1200

    Update my_wallpaper.js

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -11,13 +11,58 @@ function setup_wallpaper(pWallpaper) {
   //Grid settings
   pWallpaper.grid_settings.cell_width  = 200;
   pWallpaper.grid_settings.cell_height = 200;
-  pWallpaper.grid_settings.row_offset  = 50;
+  pWallpaper.grid_settings.row_offset  = 100;
 }
 
 function wallpaper_background() {
-  background(240, 255, 240); //light honeydew green colour
+  background(83, 212, 245); //pond blue
 }
 
 function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
-  rect(40 ,40, rect_width, rect_height);
+  angleMode()
+  
+  fill(50, 130, 44)//dark green
+
+  noStroke()
+  ellipse(100, 150, 150, 50)//lilypad
+  
+  stroke(1)
+  fill(148, 230, 135)//green
+  
+  beginShape()//body
+  vertex(100, 50)
+  vertex(25, 120)
+  vertex(50, 150)
+  vertex(150, 150)
+  vertex(175, 120)
+  endShape(CLOSE)
+
+  line(90, 150, 90, 135)//back legs
+  line(110, 150, 110, 135)
+  line(90, 135, 75, 125)
+  line(110, 135, 125, 125)
+
+  rect(50, 75, 25, 85)//front legs
+  rect(125, 75, 25, 85)
+
+  ellipse(100, 75, 125, 75)//head
+
+  fill(255)//white
+
+  circle(60, 40, 30)//eyes
+  circle(140, 40, 30)
+
+  fill(0)//black
+
+  ellipse(60, 40, 5, 10)//pupils
+  ellipse(140, 40, 5, 10)
+
+  fill(186, 194, 255)//purple
+
+  arc(60, 40, 30, 30, 0, 180, CHORD)//eyelids
+  arc(140, 40, 30, 30, 0, 180, CHORD)
+
+  noFill()
+  triangle(100, 60, 90, 70, 110, 70)//nose
+  arc(100, 75, 90, 50, 0, 180)//mouth
 }
